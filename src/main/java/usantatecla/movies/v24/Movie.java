d1fa4431c8c7342c338e5ea9f8be2458f1d7aa3d
victormartindz <victor.martin.dz@gmail.com>
2025-05-23 12:53:52 +0200

    Add default behaviour to getCharge in Movie

--- src/main/java/usantatecla/movies/v24/Movie.java
+++ src/main/java/usantatecla/movies/v24/Movie.java
@@ -1,14 +1,24 @@
 package usantatecla.movies.v24;
 
-public abstract class Movie {
+public class Movie {
 	private static final int FREQUENT_RENTER_POINTS = 1;
+	private static final int DAYS_RENTED_THRESHOLD = 2;
+	private static final double CHARGE = 2.0;
+	private static final double EXTRA_CHARGE = 1.5;
+
 	private String title;
 
 	public Movie(String title) {
 		this.title = title;
 	}
 
-	public abstract double getCharge(int daysRented);
+	public double getCharge(int daysRented) {
+		double result = Movie.CHARGE;
+		if (daysRented > Movie.DAYS_RENTED_THRESHOLD) {
+			result += (daysRented - Movie.DAYS_RENTED_THRESHOLD) * Movie.EXTRA_CHARGE;
+		}
+		return result;
+	}
 
 	public int getFrequentRenterPoints(int daysRented) {
 		return FREQUENT_RENTER_POINTS;
